e5eb9a77907d3464ec171e9659645af1a8e0dd49
Данила Тяпкин <dtapkin6@gmail.com>
2023-03-17 18:47:15 +0300

    Добавьте файлы проекта.

---
+++ Chitat_S_faila/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        try
+        {
+            StreamReader fIn = new(@"C:/Users/DELL/Desktop/hehehe.txt");
+
+            string[] elements = fIn.ReadToEnd().Split(',');
+
+            foreach (var i in elements)
+                Console.WriteLine(i);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OutOfMemoryException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
